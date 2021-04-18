e77734d81da71398f8f25eda1d24dfe466e1cf5b
ericcoker18 <ericcoker18@Yahoo.com>
2021-04-18 15:50:34 -0500

    Week 3 day 1

---
+++ CS321_W3D2_BookAPI/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W3D2_BookAPI.Models
+{
+    public class Author
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
